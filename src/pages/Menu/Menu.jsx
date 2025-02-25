da9445e2cd3b47dcaace7bf28290216ad007181e
Frenguin <104327737+Frenguin@users.noreply.github.com>
2025-02-25 12:29:40 -0600

    worked on menu page

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -8,15 +8,15 @@ function Menu(){
                     <span id='menu-title'>Menu</span>
                 </div>
             </div>
-            <div className='row d-flex justify-content-center gap-4' id='menu-container-foods-1'>
-                <div className='col-2 bg-primary text-center'>Appetizers</div>
-                <div className='col-2 bg-success text-center'>Burgers</div>
-                <div className='col-2 bg-secondary text-center'>Chicken</div>
+            <div className='row d-flex justify-content-center gap-4 mt-4 mb-4' id='menu-container-foods-1'>
+                <div className='col-2 bg-primary text-center menu-type'>Appetizers</div>
+                <div className='col-2 bg-success text-center menu-type'>Burgers</div>
+                <div className='col-2 bg-secondary text-center menu-type'>Chicken</div>
             </div>
-            <div className='row d-flex justify-content-center gap-4' id='menu-container-foods-2'>
-                <div className='col-2 bg-primary text-center'>Sides</div>
-                <div className='col-2 bg-success text-center'>Salads</div>
-                <div className='col-2 bg-secondary text-center'>Drinks</div>
+            <div className='row d-flex justify-content-center gap-4 mt-4 mb-4' id='menu-container-foods-2'>
+                <div className='col-2 bg-primary text-center menu-type'>Sides</div>
+                <div className='col-2 bg-success text-center menu-type'>Salads</div>
+                <div className='col-2 bg-secondary text-center menu-type'>Drinks</div>
             </div>
         </div>
     )
